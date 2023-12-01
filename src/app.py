f2106cceccc94ed7d9aa046a52051bcad0948183
mtnorg <151709872+mtnorg@users.noreply.github.com>
2023-12-01 11:37:27 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/")
+"""Module providing a function printing python version."""
 def index() :
-
     
-    return "Hello World"
+ 4   return "Hello World"
 if __name__=="__main__":
     app.run()
